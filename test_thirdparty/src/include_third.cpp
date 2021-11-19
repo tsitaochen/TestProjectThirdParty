a1c45898a9864279194be5dcc5fe94e433d45aaf
chenzt1993 <455563656@qq.com>
2021-11-19 20:03:26 +0800

    not ok

---
+++ test_thirdparty/src/include_third.cpp
@@ -0,0 +1,8 @@
+#include <iostream>
+#include "include_third.h"
+
+namespace thirdparty_tsitao {
+    void print_third(int a) {
+        std::cout << "hello world is "  << a << std::endl;
+    }
+}
---
+++ test_thirdparty/src/include_third.h
@@ -0,0 +1,8 @@
+#ifndef INCLUDE_THIRD_H_
+#define INCLUDE_THIRD_H_
+
+namespace thirdparty_tsitao {
+    void print_third(int a);
+}
+
+#endif /* INCLUDE_THIRD_H_ */
