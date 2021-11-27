208c43c6eb1a6e3aac73854a2aff7b4f76c08063
chenzt1993 <455563656@qq.com>
2021-11-27 13:34:00 +0800

    delete code

--- test_thirdparty/src/include_third.cpp
+++
@@ -1,8 +0,0 @@
-#include <iostream>
-#include "include_third.h"
-
-namespace thirdparty_tsitao {
-    void print_third(int a) {
-        std::cout << "hello world is "  << a << std::endl;
-    }
-}
--- test_thirdparty/src/include_third.h
+++
@@ -1,8 +0,0 @@
-#ifndef INCLUDE_THIRD_H_
-#define INCLUDE_THIRD_H_
-
-namespace thirdparty_tsitao {
-    void print_third(int a);
-}
-
-#endif /* INCLUDE_THIRD_H_ */
